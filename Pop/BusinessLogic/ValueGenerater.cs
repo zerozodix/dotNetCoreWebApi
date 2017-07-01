d4c18914ac8c8761e42ff25cb71832a63974d3b7
Zerozodix <KenNeky@Zerozodixs-MacBook-Pro.local>
2017-07-01 12:49:18 +0700

    Initail .net core api

---
+++ Pop/BusinessLogic/ValueGenerater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pop.BusinessLogic
+{
+    public class ValueGenerater
+    {
+        private static async Task<List<string>> GetDataValueFirst() => new List<string> {"abc", "defg"};
+
+        private static async Task<List<string>> GetDataValueMiddle() => new List<string> {"hij", "lmno"};
+
+        private static async Task<List<string>> GetDataValueLast() => new List<string> {"pqr", "stwv"};
+
+        public async Task<List<string>> GetAllValue()
+        {
+            var result = (await Task.WhenAll(GetDataValueFirst(), GetDataValueMiddle(), GetDataValueLast())).SelectMany(item => item).ToList();
+            return result;
+        }
+    }
+}
---
+++ Pop/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pop.BusinessLogic;
+
+namespace Pop.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ValueGenerater _valueGenerator = new ValueGenerater();
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var result = await _valueGenerator.GetAllValue();
+            return result;
+        }
+    }
+}
